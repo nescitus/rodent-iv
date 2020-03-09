7ea82b5e53db912d74abc6953d13161944498011
Bernhard C. Maerz <bcm314-github@yahoo.de>
2020-03-09 18:21:04 +0100

    replaced >’< with >'< (-Winvalid-source-encoding)

--- sources/src/taunt.cpp
+++ sources/src/taunt.cpp
@@ -1,3 +1,21 @@
+/*
+Rodent, a UCI chess playing engine derived from Sungorus 1.4
+Copyright (C) 2009-2011 Pablo Vazquez (Sungorus author)
+Copyright (C) 2011-2019 Pawel Koziol
+Copyright (C) 2020-2020 Bernhard C. Maerz
+
+Rodent is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Rodent is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "rodent.h"
 #include <iostream>
 #include <string>
@@ -63,7 +81,7 @@ const std::string crushingTaunts[CRUSHING_TAUNTS_NO] = {
 };
 
 const std::string winningTaunts[WINNING_TAUNTS_NO] = {
-    "When you absolutely don’t know what to do anymore, it is time to panic",
+    "When you absolutely don't know what to do anymore, it is time to panic",
     "It's just you against me, so you are outnumbered",
     "Don't play the victim of circumstances you created",
     "Sorry. Yesterday was deadline for all the complaints",
@@ -115,7 +133,7 @@ const std::string balanceTaunts[BALANCE_TAUNTS_NO] = {
     "It looks close... for now",
     "Seems I just met my equal",
     "Equality is not a bad thing",
-    "If your opponent offers you a draw, try to work out why he thinks he’s worse off",
+    "If your opponent offers you a draw, try to work out why he thinks he's worse off",
 };
 
 const std::string generalTaunts[GENERAL_TAUNTS_NO] = {
@@ -123,17 +141,17 @@ const std::string generalTaunts[GENERAL_TAUNTS_NO] = {
 "A bad plan is better than none at all",
 "Help your pieces so they can help you",
 "Chess is not like life... it has rules!",
-"Good positions don’t win games, good moves do",
+"Good positions don't win games, good moves do",
 "Excellence at Chess is one mark of a scheming mind",
 "When in doubt... play Chess!",
 "I play the opening like a book, the middle game like a magician, and the endgame like a machine",
 "All I want to do, ever, is just play Chess",
 "When you see a good move, look for a better one",
 "Chess is struggle, chess is battles",
-"One doesn’t have to play well, it’s enough to play better than your opponent",
-"I don’t believe in psychology. I believe in good moves",
+"One doesn't have to play well, it's enough to play better than your opponent",
+"I don't believe in psychology. I believe in good moves",
 "Chess, like love, like music, has the power to make men happy",
-"Chess doesn’t drive people mad, it keeps mad people sane",
+"Chess doesn't drive people mad, it keeps mad people sane",
 "Chess is the struggle against the error",
 "If you spend word for word with me, I shall make your wit bankrupt",
 "I am the rat king!",
@@ -149,8 +167,8 @@ const std::string generalTaunts[GENERAL_TAUNTS_NO] = {
 "What's coming will come",
 "If I put you in a win-win situation, you wil still lose",
 "Sometimes you gotta play a fool", // blunder
-"I’ll try being nicer, if you try being smarter",
-"Don’t regret doing things, regret getting caught",
+"I'll try being nicer, if you try being smarter",
+"Don't regret doing things, regret getting caught",
 "Let's see how good you really are",
 "Time to tango!",
 "Ancestors, guide my weapon!",
@@ -179,8 +197,8 @@ const std::string disadvantageTaunts[DISADVANTAGE_TAUNTS_NO] = {
     "I don't like this game",
     "I bet I can still survive",
     "Playing like minister of defence from now on!",
-    "Don’t even mention losing to me. I can’t stand to think of it",
-    "My opponents make good moves too. Sometimes I don’t take these things into consideration",
+    "Don't even mention losing to me. I can't stand to think of it",
+    "My opponents make good moves too. Sometimes I don't take these things into consideration",
     "Wait a moment, I must go to the toilet to check my moves on the phone",
     "Killing me softly...",
     "I will win by sheer power of will",
@@ -221,7 +239,7 @@ const std::string captureTaunts[CAPTURE_TAUNTS_NO] = {
 "Ka-BOOM!",
 "Hands off my prey!",
 "I did it for the greater good",
-"It’s always better to sacrifice your opponent’s men",
+"It's always better to sacrifice your opponent's men",
 "I like that one",
 "Here goes another one",
 "Another one bites the dust",
