a70e18f2c67b5ca21a2e066274226b549b3e7c6e
Bernhard C. Maerz <bcm314-github@yahoo.de>
2020-03-22 15:42:12 +0100

    chess960 - get castle info from FEN

--- sources/src/setboard.cpp
+++ sources/src/setboard.cpp
@@ -84,22 +84,88 @@ void POS::SetPosition(const char *epd) {
     if (*epd == '-')
         epd++;
     else {
-        if (*epd == 'K') {
-            mCFlags |= W_KS;
-            epd++;
+        eSquare Castle_R;
+
+        Castle_W_K = (eSquare) KingSq(WC);
+        Castle_B_K = (eSquare) KingSq(BC);
+
+        for (int i = 4; i > 0; i -= 1) {
+
+            if ((*epd >= 'A') && (*epd <= 'H')) {
+                Castle_R = (eSquare) (0 + (*epd - 'A'));
+
+                if (Castle_R > Castle_W_K) {
+                    Castle_W_RK = Castle_R;
+                    mCFlags |= W_KS;
+                } else {
+                    Castle_W_RQ = Castle_R;
+                    mCFlags |= W_QS;
+                }
+                epd++;
+            }
+
+            if ((*epd >= 'a') && (*epd <= 'h')) {
+                Castle_R = (eSquare) (56 + (*epd - 'a'));
+
+                if (Castle_R > Castle_B_K) {
+                    Castle_B_RK = Castle_R;
+                    mCFlags |= B_KS;
+                } else {
+                    Castle_B_RQ = Castle_R;
+                    mCFlags |= B_QS;
+                }
+                epd++;
+            }
+
+            if (*epd == 'K') {
+                mCFlags |= W_KS;
+                epd++;
+                for (i=0 ; i < 8 ; i++)
+                    if (Rooks(WC) & mClBb[WC] & SqBb(i))
+                        Castle_W_RK = (eSquare) (i);
+            }
+            if (*epd == 'Q') {
+                mCFlags |= W_QS;
+                epd++;
+                for (i=7 ; i >= 0 ; i--)
+                    if (Rooks(WC) & mClBb[WC] & SqBb(i))
+                        Castle_W_RQ = (eSquare) (i);
+            }
+            if (*epd == 'k') {
+                mCFlags |= B_KS;
+                epd++;
+                for (i=0 ; i < 8 ; i++)
+                    if (Rooks(BC) & mClBb[BC] & SqBb(56 + i))
+                        Castle_B_RK = (eSquare) (56 + i);
+            }
+            if (*epd == 'q') {
+                mCFlags |= B_QS;
+                epd++;
+                for (i=7 ; i >= 0 ; i--)
+                    if (Rooks(BC) & mClBb[BC] & SqBb(56 + i))
+                        Castle_B_RQ = (eSquare) (56 + i);
+            }
         }
-        if (*epd == 'Q') {
-            mCFlags |= W_QS;
-            epd++;
-        }
-        if (*epd == 'k') {
-            mCFlags |= B_KS;
-            epd++;
-        }
-        if (*epd == 'q') {
-            mCFlags |= B_QS;
-            epd++;
-        }
+
+        // "MoveToStr" did not know which side to move, so correct them also for the unneeded color
+        // Only need Castle_W_RQ and Castle_W_RK, but make proper things:
+
+        if (!(mCFlags & W_KS))
+            Castle_W_RK = (eSquare) ( 0 + File(Castle_B_RK));
+        if (!(mCFlags & W_QS))
+            Castle_W_RQ = (eSquare) ( 0 + File(Castle_B_RQ));
+        if (!(mCFlags & B_KS))
+            Castle_B_RK = (eSquare) (56 + File(Castle_W_RK));
+        if (!(mCFlags & B_QS))
+            Castle_B_RQ = (eSquare) (56 + File(Castle_W_RQ));
+
+        // and for completeness:
+        if (!(mCFlags & (W_KS | W_QS)))
+            Castle_W_K = (eSquare) ( 0 + File(Castle_B_K));
+        if (!(mCFlags & (B_KS | B_QS)))
+            Castle_B_K = (eSquare) (56 + File(Castle_W_K));
+
+        Init960();
     }
     epd++;
     if (*epd == '-')
