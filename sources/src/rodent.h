83d7fe2741c7473105347e794c172fbb74386b8d
Gerhard Kalab <gkalab@users.noreply.github.com>
2020-10-11 00:15:35 +0200

    Add UCI option "EngineHome" if basic.ini was not found
    Adds the possibility to set the engine home directory via an UCI option.
    Useful on Android when the personalities and books directories are
    located in one of the apps private files directory

--- sources/src/rodent.h
+++ sources/src/rodent.h
@@ -1117,6 +1117,7 @@ extern bool SkipBeginningOfLog;
 
 void FullPathEndSlash(std::wstring &pathWStr);
 void SetRodentHomeDir();
+bool DirOrFileExists(const char* path);
 void CreateRodentHome(const char *RodentDir);
 void ChangePersonalitySet(int persSetNo);
 
